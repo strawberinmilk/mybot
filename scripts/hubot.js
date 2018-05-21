fd6a484e602db788f5f8477eb78e95adde037646
rinkei <rinkei.krt6006@gmail.com>
2018-05-21 22:43:54 +0900

    4hourSend

--- scripts/hubot.js
+++ scripts/hubot.js
@@ -98,7 +98,7 @@ module.exports = async robot => {
     msg.send(`!(num) 曜日ごとの時間割と曜日タスクを返します/n!!今日の時間割、曜日タスク、天気を返します\n!w 天気を返します`)
   })
 
-  new cron('0 0 22 * * *', () => {
+  new cron('0 0 4/22 * * *', () => {
     let date = new Date
     date.setDate(date.getDate() + 1);
     date = date.getDay()
