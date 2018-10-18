d7fa6f3442671f29c144398452841e0149aec161
rinkei <rinkei.krt6006@gmail.com>
2018-10-18 15:44:26 +0900

    repairLightUrl

--- scripts/raspberrypi.js
+++ scripts/raspberrypi.js
@@ -5,14 +5,14 @@ const pin = 25;
 module.exports = async robot => {
   robot.hear(/^lighton$/i, function (msg) {
     request.get({
-      url: `192.168.0.74/?{"pin"${pin}:,"num":1}`,
+      url: `192.168.0.74:9001/?{"pin":${pin},"num":1}`,
     }, function (error, response, body) {
     })
     msg.send('ライトをつけます')
   });
   robot.hear(/^lightoff$/i, function (msg) {
     request.get({
-      url: `192.168.0.74/?{"pin":${pin},"num":0}`,
+      url: `192.168.0.74:9001/?{"pin":${pin},"num":0}`,
     }, function (error, response, body) {
     })
     msg.send('ライトを消します')
