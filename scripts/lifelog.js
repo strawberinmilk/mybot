71768f2197fedfc78822f54e15b8b27c95839844
rinkei-krt6006 <rinkei.krt6006@gmail.com>
2019-11-19 10:00:35 +0900

    leave

--- scripts/lifelog.js
+++ scripts/lifelog.js
@@ -1,10 +1,12 @@
 const fs = require('fs')
 const http = require('http')
+const request = require('request')
+
+const pin = 25
 
 let wifiConnect = null
 let wifiTime = 0
 let doorTime = 0
-
 let leave = false
 
 const log = text =>{
@@ -18,15 +20,30 @@ module.exports = async robot => {
     }
   }
 
-  robot.hear(/wifilog/gi,msg=>{
-    let text = msg.message.text
-    console.log('wifilog')
-    text = text.replace(/to|from/gi,'')
-    log(text)
-    wifiTime = (new Date).getTime()
-    wifiConnect = text.replace(/wifilog|\s/gi,'')
-    check()
+  robot.hear(/lighton/i, (msg) => {
+    request.get({
+      url: `http://192.168.0.62:9001/?{"pin":${pin},"num":1}`,
+    }, (error, response, body) => {
+    })
+    msg.send('ライトをつけます')
   })
+  robot.hear(/lightoff/i, (msg) => {
+    request.get({
+      url: `http://192.168.0.62:9001/?{"pin":${pin},"num":0}`,
+    }, (error, response, body) => {
+    })
+    msg.send('ライトを消します')
+  })
+
+  //robot.hear(/wifilog/gi,msg=>{
+  //  let text = msg.message.text
+  //  console.log('wifilog')
+  //  text = text.replace(/to|from/gi,'')
+  //  log(text)
+  //  wifiTime = (new Date).getTime()
+  //  wifiConnect = text.replace(/wifilog|\s/gi,'')
+  //  check()
+  //})
 
   const doorlog = text =>{
     log(text)
@@ -35,38 +52,37 @@ module.exports = async robot => {
   }
 
   
-  //leavearrive
+  //leave
   robot.hear(/leave/gi,msg=>{
-    console.log('leave')
-    leave = true
-    //let text = msg.message.text
-    //fs.appendFileSync('./data/lalog.csv',`${(new Date).toLocaleString()},${text},\n`,'utf8')
-    //if(text.match(/reave/gi)){
-    //  setTimeout(()=>{
-    //    request.get({
-    //      url: `http://192.168.0.62:9001/?{"pin":${pin},"num":1}`,
-    //    }, (error, response, body)=> {
-    //    })
-    //  },600000)
-    //}
+    clearTimeout(leave)
+    leave = setTimeout(()=>{
+      fs.appendFileSync('./data/lalog.csv',`${(new Date).toLocaleString()},leave,\n`,'utf8')
+      request.get({
+        url: `http://192.168.0.62:9001/?{"pin":${pin},"num":1}`,
+      }, (error, response, body)=> {
+      })
+      clearTimeout(leave)
+      leave = 'leave'
+    },600000)
+    msg.send('leave')
   })
-  
 
   robot.hear(/lifelog dump/gi ,(msg)=>{
     msg.send(fs.readFileSync('./data/lifelog.csv','utf8'))
   })
 
   http.createServer((req, res) => {
-    let r;
+    let r
     try{
-      r = JSON.parse(decodeURIComponent(req.url.replace(/\/|\?/gi,"")));
+      r = JSON.parse(decodeURIComponent(req.url.replace(/\/|\?/gi,"")))
     }catch(e){
     }
     console.log(r)
     robot.send({ room: r.channel }, r.text)
     if(r.text.match(/doorlog/gi)) doorlog(r.text)
-    if(r.text.match(/close/gi && leave)){
-      leave = false
+    if(r.text.match(/close/gi && leave==='leave')){
+      clearTimeout(leave)
+      leave = 'arrive'
         request.get({
         url: `http://192.168.0.62:9001/?{"pin":${pin},"num":1}`,
       }, (error, response, body)=> {
