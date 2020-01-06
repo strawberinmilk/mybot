5f9b2cce5aef587ecb60ce4722559a4875824506
rinkei-krt6006 <rinkei.krt6006@gmail.com>
2020-01-06 10:26:47 +0900

    repairDoorlogPoor

--- scripts/lifelog.js
+++ scripts/lifelog.js
@@ -83,7 +83,7 @@ module.exports = async robot => {
     }
     console.log(r)
     //test
-    robot.send({ room: test },JSON.stringify(r) )
+    robot.send({ room: 'test' },JSON.stringify(r) )
     //test
     robot.send({ room: r.channel }, r.text)
     if(r.text.match(/doorlog/gi)) doorlog(r.text)
