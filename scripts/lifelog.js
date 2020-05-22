a9e27f99d48f662f921c3ec4a44efd92a0b4f9f0
rinkei <rinkei.krt6006@gmail.com>
2020-05-22 23:46:31 +0900

    leave動作の変更

--- scripts/lifelog.js
+++ scripts/lifelog.js
@@ -8,7 +8,7 @@ let wifiConnect = null
 let wifiTime = 0
 let doorTime = 0
 let leave = false
-let leaveTimeout = null
+//let leaveTimeout = null
 
 const log = text =>{
   fs.appendFileSync('./data/lifelog.csv',`${(new Date).toLocaleString()},${text},\n`,'utf8')
@@ -57,18 +57,22 @@ module.exports = async robot => {
   
   //leave
   robot.hear(/leave/gi,msg=>{
-    clearTimeout(leaveTimeout)
-    leaveTimeout = setTimeout(()=>{
-      fs.appendFileSync('./data/lalog.csv',`${(new Date).toLocaleString()},leave,\n`,'utf8')
-      request.get({
-        //url: `http://192.168.0.62:9001/?{"pin":${pin},"num":0}`,
-        url: `http://192.168.0.62:9001/lightoff`,
+    //clearTimeout(leaveTimeout)
+    //leaveTimeout = setTimeout(()=>{
+    //  fs.appendFileSync('./data/lalog.csv',`${(new Date).toLocaleString()},leave,\n`,'utf8')
+    //  request.get({
+    //    //url: `http://192.168.0.62:9001/?{"pin":${pin},"num":0}`,
+    //    url: `http://192.168.0.62:9001/lightoff`,
+    //  }, (error, response, body)=> {
+    //  })
+    //  leave = true
+    //  robot.send({ room: '#botchannel' }, 'leaveコマンドにより消灯')   
+    //},600000)
+    request.get({
+        url: `http://192.168.0.62:9001/?{"signal":"status","leave":true}`,
       }, (error, response, body)=> {
       })
-      leave = true
-      robot.send({ room: '#botchannel' }, 'leaveコマンドにより消灯')   
-      },600000)
-  　　msg.send('10分後に消灯手配しました')
+    msg.send('消灯手配しました')
   })
 
   robot.hear(/lifelog dump/gi ,(msg)=>{
@@ -82,21 +86,23 @@ module.exports = async robot => {
     }catch(e){
     }
     console.log(r)
+    robot.send({ room: r.channel }, r.text)
+    if(r.text.match(/doorlog/gi)) doorlog(r.text)
+    
     //test
     //robot.send({ room: 'test' },JSON.stringify(r) )
     //test
-    robot.send({ room: r.channel }, r.text)
-    if(r.text.match(/doorlog/gi)) doorlog(r.text)
-    if(r.text.match(/open/gi && leave)){
-      clearTimeout(leaveTimeout)
-      leave = false
-      request.get({
-        //url: `http://192.168.0.62:9001/?{"pin":${pin},"num":1}`,
-        url: `http://192.168.0.62:9001/lighton`,
-      }, (error, response, body)=> {
-        robot.send({ room: '#botchannel' }, 'leave===true&&doorOpenにより点灯')   
-      })
-    }
+
+    //if(r.text.match(/open/gi && leave)){
+    //  //clearTimeout(leaveTimeout)
+    //  leave = false
+    //  request.get({
+    //    //url: `http://192.168.0.62:9001/?{"pin":${pin},"num":1}`,
+    //    url: `http://192.168.0.62:9001/lighton`,
+    //  }, (error, response, body)=> {
+    //    robot.send({ room: '#botchannel' }, 'leave===true&&doorOpenにより点灯')   
+    //  })
+    //}
     res.writeHead(200, {'Content-Type': 'text/plain'})
     res.end('sucsess')
   }).listen(9002)
